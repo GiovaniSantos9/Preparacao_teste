ccec24906c5e90586e2ff5b317cc173c1357c3ba
giovaniS.dev <jiovanisantos9@gmail.com>
2024-06-04 13:28:39 -0100

    add_tasks.txt

--- tasks.py
+++ tasks.py
@@ -1,5 +1,17 @@
 def main():
     print("Personal Task Manager")
+        
+def add_task(task):
+    tasks.append({"task": task, "completed": False})
+
+def main():
+    while True:
+        command = input("Comando (add/exit): ")
+        if command == "add":
+            task = input("Tarefa: ")
+            add_task(task)
+        elif command == "exit":
+            break
 
 if __name__ == "__main__":
     main()
